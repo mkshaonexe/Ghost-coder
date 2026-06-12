b43b0e9b6c6ce27238a74ec645c46c4759020042
MK Shaon <mkshaonnew31@gmail.com>
2026-06-12 09:09:54 +0600

    Start CLIServer socket server in Ghost_CoderApp

--- Ghost Coder/Ghost Coder/Ghost_CoderApp.swift
+++ Ghost Coder/Ghost Coder/Ghost_CoderApp.swift
@@ -14,6 +14,7 @@ struct Ghost_CoderApp: App {
     private var windowMonitor: WindowMonitor
     private var hotkey: GlobalHotkey
     private var mainWindowController: MainWindowController
+    private var cliServer: CLIServer
 
     init() {
         let s = GhostState()
@@ -21,16 +22,19 @@ struct Ghost_CoderApp: App {
         let w = WindowMonitor(state: s)
         let h = GlobalHotkey(state: s, interceptor: i)
         let m = MainWindowController(state: s)
+        let c = CLIServer(state: s)
         
         _state = StateObject(wrappedValue: s)
         self.interceptor = i
         self.windowMonitor = w
         self.hotkey = h
         self.mainWindowController = m
+        self.cliServer = c
         
         i.start()
         w.start()
         h.register()
+        c.start()
         DispatchQueue.main.async {
             m.show()
         }
